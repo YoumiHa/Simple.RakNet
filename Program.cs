5feee5f8ab1e829d4a022770282d237c0e6f3ce5
YoumiHa <140812890+YoumiHa@users.noreply.github.com>
2024-07-17 18:34:14 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Simple.RakNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
